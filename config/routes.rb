4bb4e0447b11c27c590d1af097a0a90baf7973a8
Casey Stinnett <stinnett.casey@gmail.com>
2018-02-26 15:49:49 -0700

    added users controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
   root "home#index"
 end
