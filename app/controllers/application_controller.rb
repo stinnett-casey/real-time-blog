91ebe8ca40cfa35f93cd7759be1cd961100d42a3
Casey Stinnett <stinnett.casey@gmail.com>
2018-02-27 10:45:52 -0700

    devise set up, login and sign up pages made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  def resource_name
+    :user
+  end
+  
+  def resource
+    @resource ||= User.new
+  end
+
+  def resource_class
+    User
+  end
+  
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  helper_method :resource_name, :resource, :devise_mapping, :resource_class
 end
