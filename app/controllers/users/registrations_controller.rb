97b56947e03aba4fa652c4fb8782c21436f04355
Casey Stinnett <stinnett.casey@gmail.com>
2018-02-27 15:17:21 -0700

    password reset styled and working

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-frozen_string_literal: true
+# frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
@@ -55,7 +55,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super(resource)
   end
 
-  The path used after sign up for inactive accounts.
+  # The path used after sign up for inactive accounts.
   def after_inactive_sign_up_path_for(resource)
     super(resource)
   end
